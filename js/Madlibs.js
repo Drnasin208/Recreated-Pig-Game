03a630d9b6e26b7eade78c1c42a3e85e74253672
Drnasin208 <60597312+Drnasin208@users.noreply.github.com>
2021-04-12 19:56:49 +0200

    Add files via upload

--- js/Madlibs.js
+++ js/Madlibs.js
@@ -1,9 +1,12 @@
+
+
 // List of prompts for the user
 var prompts = [
   "<em>Type your name",
   "<em>Type an adjective",
   "<em>Type a noun"
 ];
+
 
 var answers = [];
 // Keep track of current prompt we're on
@@ -14,19 +17,21 @@ var nextPrompt = function () {
   //if there's no answer in the form
   if (currentPrompt != 0) {
     answers.push($("input").val());
+
   }
-  // if there is a next prompt
-  if (currentPrompt < prompts.length) {
-    // put first prompt in all html elements with class
-    $(".prompt").html(prompts[currentPrompt] + '<br><input type="text">');
-    // move the next prompt into variable currentPrompt
-    currentPrompt = currentPrompt + 1;
-  }
-  //or else if we're at the end of the array
-  else {
-    // put a new message into the html.
-    showFinal();
-  }
+      // if there is a next prompt
+      if (currentPrompt < prompts.length) {
+        // put first prompt in all html elements with class
+        $(".prompt").html(`${prompts[currentPrompt]}<br><input id="input" type="text" autofocus>`);
+        // move the next prompt into variable currentPrompt
+        currentPrompt = currentPrompt + 1;
+
+      }
+      //or else if we're at the end of the array
+      else {
+        // put a new message into the html.
+        showFinal();
+      };
 };
 
 //puts user answers into HTML
@@ -44,8 +49,15 @@ var showFinal = function () {
   //and then hide the button
   $("button").hide();
 };
+
 // run nextPrompt function when button is clicked
 $("button").click(function () {
+
+  //zadržava focus u input-u
+  $('body').click(function(){
+    $("#input").focus();
+  });
+
   nextPrompt();
 });
 
